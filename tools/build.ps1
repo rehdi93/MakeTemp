ae562bc35da36dd1b90e19844892ecee9e734127
Pedro Rodrigues <pedroolivarodrigues@outlook.com>
2018-08-16 15:57:12 -0300

    improved build script

--- tools/build.ps1
+++ tools/build.ps1
@@ -8,7 +8,6 @@ $Mode
 $SEP = [string]::new('-', 35)
 $PLATFORM = [System.Environment]::OSVersion.Platform
 
-$toolsDir = $PSScriptRoot
 $projRoot = Resolve-Path "$PSScriptRoot/.."
 $buildDir = Join-Path $projRoot "build"
 
@@ -24,26 +23,21 @@ Push-Location $buildDir
 
 # run conan
 conan install .. -s build_type=$Mode
-echo "conan returned $LASTEXITCODE" $SEP
+Write-Output "conan returned $LASTEXITCODE" $SEP
 
-# genarate CMakeCache
-if ($generate) {
-    $gen = '-G "{0}"'
-
-    switch ($PLATFORM) {
-        "Win32NT" { $gen = [string]::Format($gen, "Visual Studio 15 2017 Win64") }
-        "Unix" { $gen = [string]::Format($gen, "Unix Makefiles") <# + " -DCMAKE_BUILD_TYPE=$Mode" #> }
-        Default { $gen = '' }
-    }
-
-    $cmd = "cmake .. $gen"
-    Invoke-Expression $cmd
-    echo "cmake returned $LASTEXITCODE" $SEP
+switch ($PLATFORM) {
+    "Win32NT" { 
+        if ($generate) {
+            cmake .. -G "Visual Studio 15 2017 Win64"
+        }
+     }
+     "Unix" { cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$Mode }
 }
+Write-Output "cmake returned $LASTEXITCODE" $SEP
+
 
 # run cmake build
-# ? does '--config' make a diference on Unix Makefiles ?
 cmake --build . --config $Mode
-echo "cmake returned $LASTEXITCODE" $SEP
+Write-Output "cmake returned $LASTEXITCODE" $SEP
 
 Pop-Location
